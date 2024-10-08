8ac490a996fa3f347975ca8e71ffd7060000257c
Coutzera <coutzera@outlook.com.br>
2024-10-08 17:32:05 -0300

    Criação da SQL para buscar todos os clientes cadastrados para o RCA por dia

---
+++ SQL/consulta_clientes_por_dia.sql
@@ -0,0 +1,8 @@
+SELECT
+    *
+FROM
+    pcrotacli
+WHERE
+    1 = 1
+    AND codusur = 1471
+    AND dtproxvisita BETWEEN to_date ('08/10/2024 00:00:00', 'dd/MM/yyyy hh24:mi:ss') AND to_date  ('08/10/2024 00:00:00', 'dd/MM/yyyy hh24:mi:ss')
