7d17d390d16ee671714ae9bcc14b9fa94a7718c9
Coutzera <coutzera@outlook.com.br>
2024-10-25 16:52:51 -0300

    Inclusão da consulta de Pedidos com Beneficio Fiscal

---
+++ SQL/consulta_pedidos_com_primavera_fiscal.sql
@@ -0,0 +1,17 @@
+SELECT c.codemitente as SISTEMA, p.*
+FROM pcpedi p
+JOIN pcpedc c ON p.numped = c.numped
+WHERE p.codcli IN (
+    --Bloco onde busca todos os clientes que tem o benefício
+    SELECT codcli 
+    FROM pcclient
+    WHERE tipoempresa = 'NRPA'
+    AND codfilialnf = '02'
+    --fim do block
+)
+AND p.data > TO_DATE('01-08-2024', 'DD-MM-YYYY')
+--AND p.codprod = 2443 -- Procura por produto com benefício Fiscal
+AND p.codusur = 19 -- Código do RCA
+AND p.numped IN (19020114, 19019922) -- Aqui trocar o código dos pedidos
+--19019922, 19020114
+ORDER BY p.codprod;
